3c30e520662a8b897f7bbcf2ffdd15d59c271d88
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-03 16:20:06 +0300

    prep script improved

--- prepare-data.py
+++ prepare-data.py
@@ -1,12 +1,22 @@
-from os import listdir
-from os.path import isfile, join
+from os import listdir, rmdir, makedirs
+from os.path import isdir, isfile, join
+import shutil
 import random
 
-malwarepath = './data/malware'
-benignpath = './data/benign'
+malware_path =  './data/malware'
+benign_path =   './data/benign'
+train_path =    './data/train'
+valid_path =    './data/valid'
+train_csv =    './data/train.csv'
+valid_csv =    './data/valid.csv'
 
-malwares = [f for f in listdir(malwarepath) if isfile(join(malwarepath, f))]
-benigns = [f for f in listdir(benignpath) if isfile(join(benignpath, f))]
+if not isdir(train_path):
+        makedirs(train_path);
+if not isdir(valid_path):
+        makedirs(valid_path);
+
+malwares = [f for f in listdir(malware_path) if isfile(join(malware_path, f))]
+benigns = [f for f in listdir(benign_path) if isfile(join(benign_path, f))]
 
 random.shuffle(malwares)
 random.shuffle(benigns)
@@ -19,16 +29,26 @@ benigns_split_index = int(0.8*len(benigns))
 benigns_train = benigns[:benigns_split_index]
 benigns_valid = benigns[benigns_split_index:]
 
-train_csv = open("./data/train-label.csv", "w")
+train_csv = open(train_csv, "w")
 for malware in malwares_train:
     train_csv.write('%s,1\n' % malware)
+    shutil.move('%s/%s' % (malware_path, malware), train_path)
 for benign in benigns_train:
     train_csv.write('%s,0\n' % benign)
+    shutil.move('%s/%s' % (benign_path, benign), train_path)
 train_csv.close()
 
-valid_csv = open("./data/valid-label.csv", "w")
+valid_csv = open(valid_csv, "w")
 for malware in malwares_valid:
     valid_csv.write('%s,1\n' % malware)
+    shutil.move('%s/%s' % (malware_path, malware), valid_path)
 for benign in benigns_valid:
     valid_csv.write('%s,0\n' % benign)
+    shutil.move('%s/%s' % (benign_path, benign), valid_path)
 valid_csv.close()
+
+if isdir(malware_path) and not listdir(malware_path) :
+    rmdir(malware_path)
+
+if isdir(benign_path) and not listdir(benign_path) :
+    rmdir(benign_path)
