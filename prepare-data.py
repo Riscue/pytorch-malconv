6f5e7068405a691085f2479fad25dc2d3671a896
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-03 17:54:30 +0300

    prep script hashes the raw data

--- prepare-data.py
+++ prepare-data.py
@@ -1,5 +1,6 @@
 from os import listdir, rmdir, makedirs
 from os.path import isdir, isfile, join
+import hashlib
 import shutil
 import random
 
@@ -9,6 +10,13 @@ train_path =    './data/train'
 valid_path =    './data/valid'
 train_csv =    './data/train.csv'
 valid_csv =    './data/valid.csv'
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 if not isdir(train_path):
         makedirs(train_path);
@@ -31,20 +39,24 @@ benigns_valid = benigns[benigns_split_index:]
 
 train_csv = open(train_csv, "w")
 for malware in malwares_train:
-    train_csv.write('%s,1\n' % malware)
-    shutil.move('%s/%s' % (malware_path, malware), train_path)
+    malware_hash = md5('%s/%s' % (malware_path, malware))
+    train_csv.write('%s,1\n' % malware_hash)
+    shutil.move('%s/%s' % (malware_path, malware), '%s/%s' % (train_path, malware_hash))
 for benign in benigns_train:
-    train_csv.write('%s,0\n' % benign)
-    shutil.move('%s/%s' % (benign_path, benign), train_path)
+    benign_hash = md5('%s/%s' % (benign_path, benign))
+    train_csv.write('%s,0\n' % benign_hash)
+    shutil.move('%s/%s' % (benign_path, benign), '%s/%s' % (train_path, benign_hash))
 train_csv.close()
 
 valid_csv = open(valid_csv, "w")
 for malware in malwares_valid:
-    valid_csv.write('%s,1\n' % malware)
-    shutil.move('%s/%s' % (malware_path, malware), valid_path)
+    malware_hash = md5('%s/%s' % (malware_path, malware))
+    valid_csv.write('%s,1\n' % malware_hash)
+    shutil.move('%s/%s' % (malware_path, malware), '%s/%s' % (valid_path, malware_hash))
 for benign in benigns_valid:
-    valid_csv.write('%s,0\n' % benign)
-    shutil.move('%s/%s' % (benign_path, benign), valid_path)
+    benign_hash = md5('%s/%s' % (benign_path, benign))
+    valid_csv.write('%s,0\n' % benign_hash)
+    shutil.move('%s/%s' % (benign_path, benign), '%s/%s' % (valid_path, benign_hash))
 valid_csv.close()
 
 if isdir(malware_path) and not listdir(malware_path) :
