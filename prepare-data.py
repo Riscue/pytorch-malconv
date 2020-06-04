03c942095118c7393848b62e6594d53eeea11555
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-04 13:22:30 +0300

    extract dex from apk

--- prepare-data.py
+++ prepare-data.py
@@ -1,15 +1,16 @@
-from os import listdir, rmdir, makedirs
-from os.path import isdir, isfile, join
 import hashlib
-import shutil
+import os
+import os.path as path
 import random
+import zipfile
 
-malware_path =  './raw-data/malware'
-benign_path =   './raw-data/benign'
-train_path =    './data/train'
-valid_path =    './data/valid'
-train_csv =    './data/train.csv'
-valid_csv =    './data/valid.csv'
+malware_path = './raw-data/malware'
+benign_path = './raw-data/benign'
+train_path = './data/train'
+valid_path = './data/valid'
+train_csv = './data/train.csv'
+valid_csv = './data/valid.csv'
+
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -18,43 +19,52 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-if not isdir(train_path):
-        makedirs(train_path);
-if not isdir(valid_path):
-        makedirs(valid_path);
 
-malwares = [f for f in listdir(malware_path) if isfile(join(malware_path, f))]
-benigns = [f for f in listdir(benign_path) if isfile(join(benign_path, f))]
+def extract_dex(source, target):
+    apk_zip = zipfile.ZipFile(source, 'r')
+    dex_zip = zipfile.ZipFile(target, mode='w')
 
-random.shuffle(malwares)
-random.shuffle(benigns)
+    dex_files = [f for f in apk_zip.namelist() if f.endswith('.dex')]
+    for dexFile in dex_files:
+        apk_zip.extract(dexFile)
+        dex_zip.write(dexFile)
+        os.remove(dexFile)
 
-malwares_split_index = int(0.8*len(malwares))
-malwares_train = malwares[:malwares_split_index]
-malwares_valid = malwares[malwares_split_index:]
 
-benigns_split_index = int(0.8*len(benigns))
-benigns_train = benigns[:benigns_split_index]
-benigns_valid = benigns[benigns_split_index:]
+def method_name(csv_file_name, path, malwares, benigns):
+    if not path.isdir(path):
+        os.makedirs(path)
 
-train_csv = open(train_csv, "w")
-for malware in malwares_train:
-    malware_hash = md5('%s/%s' % (malware_path, malware))
-    train_csv.write('%s,1\n' % malware_hash)
-    shutil.copy('%s/%s' % (malware_path, malware), '%s/%s' % (train_path, malware_hash))
-for benign in benigns_train:
-    benign_hash = md5('%s/%s' % (benign_path, benign))
-    train_csv.write('%s,0\n' % benign_hash)
-    shutil.copy('%s/%s' % (benign_path, benign), '%s/%s' % (train_path, benign_hash))
-train_csv.close()
+    csv_file = open(csv_file_name, "w")
+    for malware in malwares:
+        malware_hash = md5('%s/%s' % (malware_path, malware))
+        csv_file.write('%s,1\n' % malware_hash)
+        extract_dex('%s/%s' % (malware_path, malware), '%s/%s' % (path, malware_hash))
+    for benign in benigns:
+        benign_hash = md5('%s/%s' % (benign_path, benign))
+        csv_file.write('%s,0\n' % benign_hash)
+        extract_dex('%s/%s' % (benign_path, benign), '%s/%s' % (path, benign_hash))
+    csv_file.close()
 
-valid_csv = open(valid_csv, "w")
-for malware in malwares_valid:
-    malware_hash = md5('%s/%s' % (malware_path, malware))
-    valid_csv.write('%s,1\n' % malware_hash)
-    shutil.copy('%s/%s' % (malware_path, malware), '%s/%s' % (valid_path, malware_hash))
-for benign in benigns_valid:
-    benign_hash = md5('%s/%s' % (benign_path, benign))
-    valid_csv.write('%s,0\n' % benign_hash)
-    shutil.copy('%s/%s' % (benign_path, benign), '%s/%s' % (valid_path, benign_hash))
-valid_csv.close()
+
+if not path.isdir(train_path):
+    os.makedirs(train_path);
+if not path.isdir(valid_path):
+    os.makedirs(valid_path);
+
+malware_files = [f for f in os.listdir(malware_path) if path.isfile(path.join(malware_path, f))]
+benign_files = [f for f in os.listdir(benign_path) if path.isfile(path.join(benign_path, f))]
+
+random.shuffle(malware_files)
+random.shuffle(benign_files)
+
+malwares_split_index = int(0.8 * len(malware_files))
+malwares_train = malware_files[:malwares_split_index]
+malwares_valid = malware_files[malwares_split_index:]
+
+benigns_split_index = int(0.8 * len(benign_files))
+benigns_train = benign_files[:benigns_split_index]
+benigns_valid = benign_files[benigns_split_index:]
+
+method_name(train_csv, train_path, malwares_train, benigns_train)
+method_name(valid_csv, valid_path, malwares_valid, benigns_valid)
